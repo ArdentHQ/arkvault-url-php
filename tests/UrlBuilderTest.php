070cd481b863a28add101a941b62481242329e26
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2025-09-19 15:42:51 +0100

    adjust test

--- tests/UrlBuilderTest.php
+++ tests/UrlBuilderTest.php
@@ -125,8 +125,8 @@ it('should generate a vote url with username', function () {
 
     expect($builder->generateVote(
         validatorPublicKey: '03a461f557c88612328c8e6d69991eaa7916359dfd2c6a65fd988b672a8bb780c4',
-        username: 'alfy'
-    ))->toBe('https://app.arkvault.io/#/?coin=ARK&nethash=6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988&method=vote&publicKey=03a461f557c88612328c8e6d69991eaa7916359dfd2c6a65fd988b672a8bb780c4&username=alfy');
+        delegate: 'alfy'
+    ))->toBe('https://app.arkvault.io/#/?coin=ARK&nethash=6e84d08bd299ed97c212c886c98a57e36545c8f5d645ca7eeae63a8bd62d8988&method=vote&publicKey=03a461f557c88612328c8e6d69991eaa7916359dfd2c6a65fd988b672a8bb780c4&delegate=alfy');
 });
 
 it('should generate a vote url on a different network', function () {
